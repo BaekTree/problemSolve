c6c1f68da8e7c25d02f12ea5caadb604f542fb81
Baek <45053329+BaekTree@users.noreply.github.com>
2021-11-15 15:33:08 +0900

    Create NewYearChaos.py

---
+++ hackerRank/NewYearChaos.py
@@ -0,0 +1,27 @@
+#https://www.hackerrank.com/challenges/one-month-preparation-kit-new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-three
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumBribes' function below.
+#
+# The function accepts INTEGER_ARRAY q as parameter.
+#
+
+def minimumBribes(q):
+    # Write your code here
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
